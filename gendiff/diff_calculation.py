3788ad64a4de5d50a84e4431cdbb16445d1b85c3
Marina Vasyukova <marina.vasyukova@icloud.com>
2023-02-01 16:57:47 +0300

    Add 2 plain .json files comparison

---
+++ gendiff/diff_calculation.py
@@ -0,0 +1,31 @@
+import json
+
+def make_string(key, value):
+    if isinstance(value, bool):
+        value = str(value).lower()
+    return (f'{key}: {value}')
+
+
+def generate_diff(first_path, second_path):
+    first_file = json.load(open(first_path))
+    second_file = json.load(open(second_path))
+    result = '{\n'
+    keys_1 = set(first_file.keys())
+    keys_2 = set(second_file.keys())
+    unique_keys = sorted(list(keys_1.union(keys_2)))
+    for k in unique_keys:
+        if k in first_file and not k in second_file:
+            result += f'- {make_string(k, first_file[k])}\n'
+        elif k not in first_file and k in second_file:
+            result += f'+ {make_string(k,second_file[k])}\n'
+        else:
+            if first_file[k] == second_file[k]:
+                result += f'  {make_string(k, first_file[k])}\n'
+            else:
+                result += f'- {make_string(k, first_file[k])}\n+ {make_string(k,second_file[k])}\n'
+    result += '}'
+    return result
+
+#print(generate_diff(first_file, second_file))
+
+#print(make_string('follow', True))
