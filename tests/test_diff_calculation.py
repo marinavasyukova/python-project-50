e71f5c7ef197e908e2455d16c4844da43d88cd3f
Marina Vasyukova <marina.vasyukova@icloud.com>
2023-02-05 02:42:07 +0300

    Add test for yaml files

--- tests/test_diff_calculation.py
+++ tests/test_diff_calculation.py
@@ -2,7 +2,10 @@ from gendiff import generate_diff
 
 
 def test_generate_diff():
-    first_path = 'tests/fixtures/file1.json'
-    second_path = 'tests/fixtures/file2.json'
+    first_json = 'tests/fixtures/file1.json'
+    second_json = 'tests/fixtures/file2.json'
+    first_yaml = 'tests/fixtures/file1.yaml'
+    second_yaml = 'tests/fixtures/file2.yaml'
     result = open('tests/fixtures/result_json', 'r') 
-    assert generate_diff(first_path, second_path) == result.read()
+    assert generate_diff(first_json, second_json) == result.read()
+    assert generate_diff(first_yaml, second_yaml) == result.read()
