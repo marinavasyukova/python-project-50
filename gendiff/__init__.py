cd2d344c08bdfadbc220b42ca40b6105f78309f1
Marina Vasyukova <marina.vasyukova@icloud.com>
2023-02-04 13:05:38 +0300

    Update diff_calculation.py and init.py

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,3 +1,3 @@
 from gendiff.diff_calculation import generate_diff
 
-__all__ = ('generate_diff',)
+__all__ = ['generate_diff']
--- gendiff/diff_calculation.py
+++ gendiff/diff_calculation.py
@@ -1,5 +1,6 @@
 import json
 
+
 def make_string(key, value):
     if isinstance(value, bool):
         value = str(value).lower()
@@ -14,7 +15,7 @@ def generate_diff(first_path, second_path):
     keys_2 = set(second_file.keys())
     unique_keys = sorted(list(keys_1.union(keys_2)))
     for k in unique_keys:
-        if k in first_file and not k in second_file:
+        if k in first_file and k not in second_file:
             result += f'- {make_string(k, first_file[k])}\n'
         elif k not in first_file and k in second_file:
             result += f'+ {make_string(k,second_file[k])}\n'
@@ -26,6 +27,6 @@ def generate_diff(first_path, second_path):
     result += '}'
     return result
 
-#print(generate_diff(first_file, second_file))
+# print(generate_diff(first_file, second_file))
 
-#print(make_string('follow', True))
+# print(make_string('follow', True))
