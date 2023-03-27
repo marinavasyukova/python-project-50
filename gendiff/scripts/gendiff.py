c262241a52b9d13b799cbda0f277ecdd0b670d74
Marina Vasyukova <marina.vasyukova@icloud.com>
2023-03-28 02:17:48 +0300

    Fix linter error

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,7 +8,9 @@ def main():
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('first_file', type=str, help='path to the first file')
     parser.add_argument('second_file', type=str, help='path to the second file')
-    parser.add_argument('-f', '--format', help='set format of output (default: "stylish")', choices=['plain', 'stylish', 'json'], default='stylish')
+    parser.add_argument('-f', '--format',
+                        help='set format of output (default: "stylish")',
+                        choices=['plain', 'stylish', 'json'], default='stylish')
     args = parser.parse_args()
     print(generate_diff(args.first_file, args.second_file, args.format))
 
